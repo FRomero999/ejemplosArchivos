c6841fb526cbeb7579c9d3605a2b3e71e44e4721
Francisco <francisco.romero@cesurformacion.com>
2025-10-06 23:35:23 +0200

    Añadido Jackson

---
+++ src/main/java/org/example/UsoJackson.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UsoJackson {
+
+    public static void main(String[] args) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Alumno alumno = new Alumno("Laura", "Gómez", "12345678A");
+        System.out.println("Objeto original:");
+        System.out.println(alumno);
+
+        try {
+            mapper.writeValue(new File("alumno.json"), alumno);
+            System.out.println("\nObjeto serializado a JSON y guardado en 'alumno.json'.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Alumno alumnoLeido = mapper.readValue(new File("alumno.json"), Alumno.class);
+            /*
+             si quisiera leer una lista de alummnos:
+             List<Alumno> alumnosLeidos = mapper.readValue(
+                    new File("alumnos.json"),
+                    new TypeReference<List<Alumno>>() {}
+            );
+             */
+            System.out.println("\nObjeto leído desde JSON:");
+            System.out.println(alumnoLeido);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
